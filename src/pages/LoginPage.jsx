896a7837b53f6fa35a7ef2ea8bbbca51d0461326
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-20 23:37:19 -0300

    feat: Initiate Form in LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function LoginPage () {
   return (
-    <h2>LOGIN PAGE HERE</h2>
+   <main>
+    <form>
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" name="user" />
+      <input type="password" placeholder="Password" name="password" />
+      <button className="btn btn-primary btn-login" >Sign in</button>
+    </form>
+   </main>
   )
 }
 

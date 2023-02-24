8e8f25e6ffd8b066f3ffc571c0245ec7e9a03099
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-23 21:26:39 -0300

    feat: Apply css in LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../css/loginPage.css";
 
 function LoginPage () {
   return (
    <main>
-    <form>
+    <form class="form">
       <h2>Login</h2>
       <input type="text" placeholder="Username" name="user" />
       <input type="password" placeholder="Password" name="password" />

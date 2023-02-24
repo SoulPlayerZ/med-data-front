9ad429a29ad965a9c4cd8c1d4c652743060220c3
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-23 21:31:30 -0300

    feat: More css in LoginPage(Form)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,8 +4,10 @@ import "../css/loginPage.css";
 function LoginPage () {
   return (
    <main>
-    <form class="form">
-      <h2>Login</h2>
+    <form className="form">
+      <section className="login-title-container">
+        <h2>Login</h2>
+      </section>
       <input type="text" placeholder="Username" name="user" />
       <input type="password" placeholder="Password" name="password" />
       <button className="btn btn-primary btn-login" >Sign in</button>
